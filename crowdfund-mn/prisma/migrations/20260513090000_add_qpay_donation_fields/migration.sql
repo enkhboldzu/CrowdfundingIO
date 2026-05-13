3de3d6c7eabf45927b8f963aeaffd7ace733336a
baterdeneanarerdene09-tech <baterdeneanarerdene09@gmail.com>
2026-05-13 19:31:08 +0800

    1

--- crowdfund-mn/prisma/migrations/20260513090000_add_qpay_donation_fields/migration.sql
+++ crowdfund-mn/prisma/migrations/20260513090000_add_qpay_donation_fields/migration.sql
@@ -6,7 +6,6 @@ ALTER TABLE "Donation" ADD COLUMN "qpayShortUrl" TEXT;
 ALTER TABLE "Donation" ADD COLUMN "qpayUrls" JSONB;
 ALTER TABLE "Donation" ADD COLUMN "paidAt" TIMESTAMP(3);
 ALTER TABLE "Donation" ADD COLUMN "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
-ALTER TABLE "Donation" ALTER COLUMN "updatedAt" DROP DEFAULT;
 
 CREATE UNIQUE INDEX "Donation_qpayInvoiceId_key" ON "Donation"("qpayInvoiceId");
 CREATE INDEX "Donation_status_idx" ON "Donation"("status");
